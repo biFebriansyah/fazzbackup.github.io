0a5174213f68c1f578e0edc0bd1381fbc90b62c6
ebiFebriansyah <bukanebi@gmail.com>
2023-04-12 22:12:38 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 style={{ marginBottom: '20px' }}>fazzbackup</h1>
+                <code class="lead">
+                    <span>Designed and built by&nbsp;</span>
+                    <a href="https://github.com/gobackup/gobackup">gobackup/gobackup</a>
+                    <span>&nbsp;and modified for personal needs.</span>
+                </code>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
